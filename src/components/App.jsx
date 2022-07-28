8b13613650c9da05d9bf65b60c58e70d852af11a
Christian Duda <89409014+DudaKristian@users.noreply.github.com>
2022-07-28 16:58:57 +0300

    Update App.jsx
    LocalStorage feachure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,7 @@ import Phonebook from "./Phonebook/Phonebook";
 class App extends React.Component{
 
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+    contacts: [],
     filter: '',
   }
 
@@ -47,6 +44,18 @@ class App extends React.Component{
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== id)
     }))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+  }
+  };
+
+  componentDidMount() {
+
+    const parsedContacts = JSON.parse(localStorage.getItem("contacts"))
+    this.setState({contacts: parsedContacts})
   }
 
   render() {
